9c280b537ef363c60f0478d4f0ab601efbe9f9e0
Theodore <elvisxiaozhi@gmail.com>
2018-11-14 12:51:23 +0800

    add reverseLevelOrder

--- src/tree.h
+++ src/tree.h
@@ -37,6 +37,34 @@ struct Node
             }
         }
     }
+
+    void reverseLevelOrder(Node *root) {
+        QStack<Node *> stack;
+        QQueue<Node *> queue;
+        queue.push_back(root);
+
+        // Do something like normal level order traversal order. Following are the
+        // differences with normal level order traversal
+        // 1) Instead of printing a node, we push the node to stack
+        // 2) Right subtree is visited before left subtree
+        while (!queue.empty()) {
+            /* Dequeue node and make it root */
+            root = queue.front();
+            queue.pop_front();
+            stack.push_back(root);
+
+            for (int i = 0; i < root->child.size(); ++i) {
+                queue.push_back(root->child[i]);
+            }
+        }
+
+        // Now pop all items from stack one by one and print them
+        while (!stack.empty()) {
+            root = stack.top();
+            qDebug() << root->row << root->col;
+            stack.pop();
+        }
+    }
 };
 
 #endif // TREE_H
