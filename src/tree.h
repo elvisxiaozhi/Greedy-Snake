be920387411bd8292a9200d324d28e587a2ffedd
Theodore <elvisxiaozhi@gmail.com>
2018-12-22 20:06:07 +0800

    add smart pointer to tree file

--- src/tree.h
+++ src/tree.h
@@ -4,6 +4,13 @@
 #include <QVector>
 #include <QQueue>
 #include <QDebug>
+#include <memory>
+
+using std::vector;
+using std::shared_ptr;
+using std::make_shared;
+using std::pair;
+using std::make_pair;
 
 struct Node
 {
@@ -11,7 +18,13 @@ struct Node
     bool visited;
     QVector<Node *> child;
     Node *childNode;
+
+    vector<shared_ptr<Node> > childVec;
+    shared_ptr<Node> smartChild;
+
+
     Node(int x, int y) : row(x), col(y), childNode(nullptr) { visited = false; cost = INT_MAX; }
+
 
     void rootToLeaf(Node *node, QVector<std::pair<int, int> > path, QVector<QVector<std::pair<int, int> > > &res) {
         if (node == nullptr)
@@ -27,6 +40,32 @@ struct Node
                 rootToLeaf(node->child[i], path, res);
             }
         }
+    }
+
+    void rootToLeaf(shared_ptr<Node> node, QVector<pair<int, int> > path, QVector<QVector<pair<int, int> > > &res) {
+        if (node == nullptr)
+            return;
+
+        path.push_back(make_pair(node->row, node->col));
+        if (node->childVec.empty())
+            res.push_back(path);
+        else {
+            for (unsigned int i = 0; i < node->childVec.size(); ++i)
+                rootToLeaf(node->childVec[i], path, res);
+        }
+    }
+
+    shared_ptr<Node> createChild(shared_ptr<Node> node, int row, int col) {
+        shared_ptr<Node> tempChild = make_shared<Node>(row, col);
+        node->childVec.push_back(tempChild);
+        return tempChild;
+    }
+
+    shared_ptr<Node> createChild(shared_ptr<Node> node, int row, int col, int cost) {
+        shared_ptr<Node> tempChild = make_shared<Node>(row, col);
+        tempChild->cost = cost;
+        node->childVec.push_back(tempChild);
+        return tempChild;
     }
 
     Node *makeChild(Node *root, int row, int col) {
